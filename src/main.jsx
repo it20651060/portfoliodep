6b1e254af9d20c31574e8797e510fc569e3489d1
IT20651060 <it20651060@my.sliit.lk>
2025-08-09 20:54:03 +0530

    summary

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
 );
